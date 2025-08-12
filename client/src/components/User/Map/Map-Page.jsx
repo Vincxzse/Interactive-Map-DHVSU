04fe6058226eb64e6fce8293fb897dad96b052be
Vince Salenga <vince.salenga@gmail.com>
2025-08-12 23:32:33 +0800

    Remodeling User Page | Map in progress

--- client/src/components/User/Map/Map-Page.jsx
+++ client/src/components/User/Map/Map-Page.jsx
@@ -4,8 +4,8 @@ import Phaser from 'phaser';
 function MapPage() {
     const speedDown = 300;
     const sizes = {
-        width: 1000,
-        height: 600,
+        width: 1520,
+        height: 610,
     }
 
     class GameScene extends Phaser.Scene {
@@ -17,51 +17,76 @@ function MapPage() {
 
         preload() {
             this.load.image('bg1', '/map-assets/test.jpg');
+            this.load.image('shed1', '/map-assets/shed.png');
+            this.load.image('admin', '/map-assets/bldg1.png');
             this.load.image('avatar', '/map-assets/avatar-front.png');
+            this.load.image('doormat1', '/map-assets/doormat.png');
         }
 
         create() {
-            this.bg1 = this.add.tileSprite(0, 0, 2000, 2000, 'bg1').setOrigin(0, 0);
+            const worldWidth = 2000;
+            const worldHeight = 3000; 
+            this.bg1 = this.add.tileSprite(0, 0, worldWidth, worldHeight, 'bg1').setOrigin(0, 0);
 
-            this.player = this.physics.add.image(0, 2000 - 40, 'avatar').setOrigin(0, 0).setDisplaySize(40, 40);
+            // Admin Building
+            this.admin = this.physics.add.staticImage(820, 1500, 'admin').setOrigin(0, 0).setDepth(2);
+            this.admin.body.setSize(this.admin.width - 20, this.admin.height - 200);
+            this.admin.body.setOffset(this.admin.width - 455, this.admin.height);
+
+            // Shed
+            this.shed = this.physics.add.staticImage(500, 500, 'shed1').setOrigin(0, 0).setDepth(2);
+            this.shed.body.allowGravity = false;
+            this.shed.body.setSize(1, this.shed.height - 100);
+            this.shed.body.setOffset(this.shed.width, this.shed.height - 1190);
+
+            // Doormat
+            this.doormat1 = this.physics.add.image(1260, 1905, 'doormat1').setOrigin(0, 0).setDepth(0);
+            this.doormat1.displayHeight = 20;
+            this.doormat1.displayWidth = 50;
+            
+            // Player and colliders
+            this.player = this.physics.add.image(0, 2000 - 40, 'avatar').setOrigin(0, 0).setDisplaySize(60, 60).setDepth(1);
             this.player.body.allowGravity = false;
+            // this.physics.add.collider(this.player, this.shed);
+            this.physics.add.collider(this.player, this.admin);
+
 
             this.cameras.main.startFollow(this.player, true, 1, 1);
             
-            this.cameras.main.setBounds(0, 0, 2000, 2000);
-            this.physics.world.setBounds(0, 0, 2000, 2000);
-
+            this.cameras.main.setBounds(0, 0, worldWidth, worldHeight);
+            this.physics.world.setBounds(0, 0, worldWidth, worldHeight);
+            
+            // Keyboard movements
             this.cursor = this.input.keyboard.createCursorKeys();
             this.keys = this.input.keyboard.addKeys({
                 up: Phaser.Input.Keyboard.KeyCodes.W,
                 down: Phaser.Input.Keyboard.KeyCodes.S,
                 left: Phaser.Input.Keyboard.KeyCodes.A,
                 right: Phaser.Input.Keyboard.KeyCodes.D,
-                enter: Phaser.Input.Keyboard.KeyCodes.ENTER,
-                one: Phaser.Input.Keyboard.KeyCodes.ONE,
-                two: Phaser.Input.Keyboard.KeyCodes.TWO,
-                three: Phaser.Input.Keyboard.KeyCodes.THREE
             });
 
             this.player.setCollideWorldBounds(true);
-            this.physics.add.collider(this.player, this.room1);
-            this.physics.add.collider(this.player, this.guard);
         }
 
         update() {
+            // Keyboard Movements
             const { left, right, up, down } = this.cursor;
             const { up: w, down: s, left: a, right: d } = this.keys;
-
             let velocityX = 0;
             let velocityY = 0;
-
             if (left.isDown || a.isDown) velocityX = -this.playerSpeed;
             else if (right.isDown || d.isDown) velocityX = this.playerSpeed;
-
             if (up.isDown || w.isDown) velocityY = -this.playerSpeed;
             else if (down.isDown || s.isDown) velocityY = this.playerSpeed;
-
             this.player.setVelocity(velocityX, velocityY);
+
+            if (this.player.y > this.shed.y + this.shed.height - 40) {
+                this.shed.setDepth(0); // Behind player
+                this.player.setDepth(1);
+            } else {
+                this.shed.setDepth(2); // In front of player
+                this.player.setDepth(1);
+            }
         }
     }
 
@@ -74,7 +99,7 @@ function MapPage() {
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: { y: speedDown },
+                gravity: { y: 0 },
                 debug: true,
             },
         },
